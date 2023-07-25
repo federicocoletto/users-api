54f62398821d717587e78cd813c48ea9d5e0090f
federicocoletto <federico.coletto@fce.uncu.edu.ar>
2023-07-25 20:06:17 -0300

    arreglo home page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -7,11 +7,10 @@ const HomePage = () => {
 		<div className='home page'>
 			<div className="home__header">
 				<h1 className='home__h1'>Users APP</h1>
-				<p>Developed with my own api</p>
 			</div>
 			<div className="home__buttons">
 				<button className='home__button login' >
-					<Link to="/login" >Log in</Link>
+					<Link to="/login">Log in</Link>
 				</button>
 			</div>
 		</div>
